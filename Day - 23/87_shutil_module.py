84e5dfe97e3c687ae741e2144c3920a5d9ea230c
pulkitsaroha7 <pulkitsaroha01@gmail.com>
2024-04-07 22:33:15 +0530

    .\Day - 23"

---
+++ Day - 23/87_shutil_module.py
@@ -0,0 +1,18 @@
+# Shutil is a Python module that provides a higher level interface for working with file and directories. The name "shutil" is short for shell utility. It provides a convenient and efficient way to automate tasks that are commonly performed on files and directories.
+
+# shutil.copy(src, dst): This function copies the file located at src to a new location specified by dst. If the destination location already exists, the original file will be overwritten.
+
+# shutil.copy2(src, dst): This function is similar to shutil.copy, but it also preserves more metadata about the original file, such as the timestamp.
+
+# shutil.copytree(src, dst): This function recursively copies the directory located at src to a new location specified by dst. If the destination location already exists, the original directory will be merged with it.
+
+# shutil.move(src, dst): This function moves the file located at src to a new location specified by dst. This function is equivalent to renaming a file in most cases.
+
+# shutil.rmtree(path): This function recursively deletes the directory located at path, along with all of its contents. This function is similar to using the rm -rf command in a shell
+
+import shutil
+ 
+# shutil.copy('Exercises/KBC.py' , 'KBC.py')
+# shutil.copytree('Exercises','Exercises2')
+# shutil.move(".tutorial/file.file", "file.file")
+# shutil.rmtree("mytutorial")
